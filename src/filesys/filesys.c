bb0e6070ce27e08bdedfd3677ebe78b398d45a01
Alexei Ionov <104494493+Alexei-Ionov@users.noreply.github.com>
2024-05-03 22:55:02 -0700

    Update filesys.c

--- src/filesys/filesys.c
+++ src/filesys/filesys.c
@@ -28,7 +28,7 @@ void filesys_init(bool format) {
   if (format)
     do_format();
 
-  // free_map_open();
+  free_map_open();
 }
 
 /* Shuts down the file system module, writing any unwritten data
@@ -154,9 +154,9 @@ bool filesys_remove(const char* name) {
 /* Formats the file system. */
 static void do_format(void) {
   printf("Formatting file system...");
-  // free_map_create();
+  free_map_create();
   if (!dir_create(ROOT_DIR_SECTOR, 16, ROOT_DIR_SECTOR))
     PANIC("root directory creation failed");
-  // free_map_close();
+  free_map_close();
   printf("done.\n");
 }
